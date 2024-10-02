653a0ad35b7b1259d8f5be5031c59ce2adb1d9f0
Thomas Fuchs <fuchs-thomas93@gmx.de>
2024-10-02 11:40:14 +0200

    Solution

---
+++ react-state_emoji-checker/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [code, setCode] = useState("");
+
+  const validCode = "🐡🐠🐋";
+
+  function handleClick(emoji) {
+    setCode(code + emoji);
+    // console.log(emoji);
+    console.log(code + emoji);
+  }
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐡");
+          }}
+        >
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐋");
+          }}
+        >
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            handleClick("🐠");
+          }}
+        >
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+          console.log("Reset Code!");
+        }}
+      >
+        Reset
+      </button>
+
+      {code === validCode && <p>Valid code! 🎉</p>}
+    </div>
+  );
+}
