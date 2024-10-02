53bc0eb2ea2552c2be7e89d3a6878f9a14118fa9
Thomas Fuchs <fuchs-thomas93@gmx.de>
2024-10-02 15:56:11 +0200

    Solution

---
+++ react-with-arrays_users/src/components/Tag.jsx
@@ -0,0 +1,10 @@
+import "./Tag.css";
+
+export default function Tag({ tag }) {
+  const className = tag === "admin" ? "tag tag--highlight" : "tag";
+  return <li className={className}>{tag}</li>;
+}
+
+// 3. At last, refactor the `Tag` component: it should receive an additional
+// class called 'tag--highlight' if the `tag` prop equals the string
+// 'admin' (compare the `Tag.css` file).
