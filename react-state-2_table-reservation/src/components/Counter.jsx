5ff3f3c0764169497915462745ba50d48c2fb4a1
Thomas Fuchs <fuchs-thomas93@gmx.de>
2024-10-04 11:53:16 +0200

    change p

--- react-state-2_table-reservation/src/components/Counter.jsx
+++ react-state-2_table-reservation/src/components/Counter.jsx
@@ -1,8 +1,7 @@
-export default function Counter({ people, onAdd, onSubtract }) {
+export default function Counter({ onAdd, onSubtract }) {
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
-      <p>{people} people</p>
       <div className="counter__buttons-container">
         <button
           type="button"
